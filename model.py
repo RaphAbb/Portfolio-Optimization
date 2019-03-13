0c31d1f30690fbe92dc4230a7917ca89c8597fe1
Simon Hagege <hagege@stanford.edu>
2019-03-13 16:01:33 -0700

    ADMM is working, still to figure out the neg coefficients issue

--- model.py
+++ model.py
@@ -1,70 +1,108 @@
 import numpy as np
 import pandas as pd
-from utils import data_to_pd, cov_matrix_hand, cov_matrix_pd
+from tqdm import tqdm
+import random
+from datetime import datetime
 
 class ADMM_Solver():
 
-    def __init__(self, path, window, date, nb_iter, beta):
-        self.path = path
-        self.window = window
-        self.cov = np.zeros([10,10])
-        self.date = date
-        self.weights = np.random.rand(10,1)
-        self.lambda_ = np.random.rand(2,1)
-
-    def get_data(self, path):
+    def __init__(self, path):
+        #self.weights = np.random.rand(10,1)
         self.data = pd.read_excel(path, sheet_name = 'data_post')
         self.data = self.data.set_index('date').dropna()
         self.ret = self.data/self.data.shift(1)-1
-        self.log_ret = np.log(self.data/self.data.shift(1))
-        self.A = np.array([self.log_ret,np.ones(10)])
-'''
-TO BE UPDATED
+        self.log_ret = np.log(self.data/self.data.shift(1)).dropna()
+
     def cov_matrix(self, date, window = 120, drop_backtest = True):
         if drop_backtest:
-            data = data.drop(['MSCI'], axis = 1)
-        return np.array(data.cov())
+            data = self.log_ret.drop(['MSCI'], axis = 1)
+        data_ = data.loc[date:]
+        data_ = data_[:window]
+        return np.array(data_.cov())
 
-    def cov_matrix_hand(data, window = 120, drop_backtest = True):
+    def cov_matrix_hand(self, date, window = 120, drop_backtest = True):
         if drop_backtest:
-            data = data.drop(['MSCI'], axis = 1)
+            data = self.log_ret.drop(['MSCI'], axis = 1)
+        data = data.loc[date:]
+        data = data[:window]
         A = np.zeros([10,10])
-        n = data.shape[0]
         sum_ = np.array(data.sum(axis = 0))
-        y_bar = sum_/n
+        y_bar = sum_/window
         index_ = {0:'SPY',1:'VTI',2:'BND',3:'EMB',4:'TLT',5:'MBB',6:'IAU',7:'USO',8:'USDEUR',9:'USDJPY'}
         for i in range(10):
             for j in range(10):
                 if i == j:
-                    A[(i,i)] = np.sum(np.square(data[index_[i]] - y_bar[i]))/n
+                    A[(i,i)] = (np.square(data[index_[i]] - y_bar[i])).sum(axis=0)/window
                 else:
-                    A[(i,j)] = np.sum((data[index_[i]] - y_bar[i])*(data[index_[j]] - y_bar[j]))/n
+                    A[(i,j)] = ((data[index_[i]] - y_bar[i])*(data[index_[j]] - y_bar[j])).sum(axis=0)/window
         return A
-'''
-    def get_cov(self, by_hand, window, date)
+
+    def get_cov(self, by_hand, window, date):
         if by_hand:
-            return cov_matrix_hand(self.data)
+            self.cov = self.cov_matrix_hand(window = window, date = date)
+            self.sigma = self.cov.diagonal().T
         else:
-        	return cov_matrix_pd(self.data)
+            self.cov = self.cov_matrix(window = window, date = date)
+            self.sigma = self.cov.diagonal().T
 
-    def divide_A(self):
-        return [self.A[i:i+5] for i in range(0,10,5)]
+    def grad_x(self, x, z, u, rho):
+        return self.cov@x + rho*(x - z + u)
 
-    def divide_weights(self):
-        return [self.weights[i:i+5] for i in range(0,10,5)]
+    def grad_z(self, x, z, u, rho, lambda_star, y_star):
+        return lambda_star*self.sigma + y_star + rho*(x-z+u)
 
-    def update_l(self, l, beta):
-    	A = self.divide_A()
-    	w = self.divide_weights()
-    	L = np.zeros([2,1])
-    	for i  in range(2):
-    		L += A[i]@w[i]
-    	self.lambda_ = self.lambda_ + self.beta*(L - np.array([[1],[1]]))
-    	return self.lambda_
+    def update_u(self, u, x, z):
+        return u + (x-z)
 
-    def lc(self):
-    	L = np.zeros([2,1])
-    	for i  in range(2):
-    		L += A[i]@w[i]
-        lc = np.transpose(self.weights)@self.cov@self.weights/2 + self.lambda_@L + (c/2)*np.transpose(L)@L
-        return lc
+    #Gradient Descent Method to find min x^{k+1}, fixed alpha
+    def GDM_x(self, X, z, u, nb_iter, alpha, rho):
+        i = 0
+        while i<nb_iter:
+            X = X - alpha*self.grad_x(x = X,z = z,u = u, rho = rho)
+            i += 1
+        return X
+
+    #Gradient Descent Method to find min z^{k+1}, fixed alpha
+    def GDM_z(self, Z, x, u, nb_iter, alpha, rho, lambda_star, y_star):
+        i = 0
+        while i<nb_iter:
+            Z = Z - alpha*self.grad_z(x = x,z = Z,u = u, rho = rho, lambda_star = lambda_star, y_star = y_star)
+            i += 1
+        return Z
+
+    def solve(self, nb_iter = 10000, nb_iter_grad = 500, alpha = 0.01, rho = 0.1, lambda_star = 0.1, y_star = np.random.rand(10)):
+        random.seed(1515)
+        i = 0
+        L = []
+        x = np.random.rand(10)
+        x = x/np.sum(x)
+        assert np.sum(x) == 1.0
+        z = np.random.rand(10)
+        z = z/np.sum(z)
+        assert np.sum(z) == 1.0
+        u = np.random.rand(10)
+        print('-'*80)
+        print("ADMM Solver")
+        print("Inialization of x: \n",x)
+        print("Inialization of z: \n",z)
+        print("Inialization of u: \n",u)
+        print('-'*80)
+        for i in tqdm(range(nb_iter)):
+            x = self.GDM_x(X = x, z = z, u = u, nb_iter=nb_iter_grad, alpha = alpha, rho = rho)
+            #print("This is x: \n", x)
+            z = self.GDM_z(Z = z, x = x, u = u, nb_iter=nb_iter_grad, alpha = alpha, rho = rho, lambda_star = lambda_star, y_star = y_star)
+            #print("This is z: \n", z)
+            u = self.update_u(u = u, x = x, z = z)
+            #print("This is u: \n", u)
+            i+=1
+            x = x/np.sum(x)
+            z = z/np.sum(z)
+        return x,z
+
+if __name__ == '__main__':
+    path = './data/BloombergData_2004_2019.xlsx'
+    admm = ADMM_Solver(path)
+    admm.get_cov(by_hand = False, window = 120, date = pd.Timestamp('2019-2-28'))
+    print(admm.solve(nb_iter = 10000, nb_iter_grad = 500, alpha = 0.0001, rho = 0.0001, lambda_star = 0.001))
+
+
